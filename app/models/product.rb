a44cb606e30eb7a7bebe8a0a0f15d9561025d725
Bowen <huangbowen521@gmail.com>
2012-11-15 23:42:42 +0800

    create store controller.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,14 @@ class Product < ActiveRecord::Base
 
   validates_presence_of :title, :image_url, :description
   validates_numericality_of :price
-  validates_uniqueness_of :title
+  validates_uniqueness_of :title, :message => 'have been used'
+  validates_length_of :title, :minimum => 10, :too_short => 'should at least 10 character'
   validates_format_of :image_url, :with => %r{\.(gif|jpg|png)$}i, :message => 'must be a URL for GIF, JPG' + 'or PNG image.'
   validate :price_must_be_at_least_a_cent
+
+  def self.find_products_for_sale
+    find(:all, :order => 'title')
+  end
 
   protected
   def price_must_be_at_least_a_cent
--- db/migrate/20121114150054_add_test_data.rb
+++ db/migrate/20121114150054_add_test_data.rb
@@ -1,6 +1,9 @@
 class AddTestData < ActiveRecord::Migration
   def up
-
+    Product.delete_all
+    Product.create(:title => 'Productive programmer', :description => %{This is a good book.}, :image_url => 'http://media-origin.pragprog.com/titles/rails3/code/depot_c/public/images/auto.jpg', :price => '100.00')
+    Product.create(:title => 'Head first design', :description => %{This is a good book.}, :image_url => 'http://media-origin.pragprog.com/titles/rails3/code/depot_c/public/images/auto.jpg', :price => '100.00')
+    Product.create(:title => 'Working effectively with legacy code', :description => %{This is a good book.}, :image_url => 'http://media-origin.pragprog.com/titles/rails3/code/depot_c/public/images/auto.jpg', :price => '100.00')
   end
 
   def down
